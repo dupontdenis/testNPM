eddc5446a3427e4e0ceac4258c602a290214c5e8
Super Dupont <dupontdenis@users.noreply.github.com>
2023-09-23 15:25:53 +0200

    Create app.mjs
    Add a module app.mjs

---
+++ app.mjs
@@ -0,0 +1,23 @@
+import fetch from "node-fetch";
+import _ from "lodash";
+import path from "path";
+import fs from "fs";
+
+import * as url from "url";
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = url.fileURLToPath(new URL(".", import.meta.url));
+
+async function run() {
+  const response = await fetch("https://dev.to/api/articles?state=rising");
+  const json = await response.json();
+  const sorted = _.orderBy(json, ["public_reactions_count"], ["desc"]);
+  const top5 = _.take(sorted, 3);
+
+  const filePrefix = new Date().toISOString().split("T")[0];
+  fs.writeFileSync(
+    path.join(__dirname, `${filePrefix}-feed.json`),
+    JSON.stringify(top5, null, 2)
+  );
+}
+
+run();
